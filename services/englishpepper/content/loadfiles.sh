1cc92f0311a7640db49bf7c70a733bd7afa211d5
Ubuntu <ubuntu@domU-12-31-38-04-6A-5A.compute-1.internal>
2012-08-23 11:00:38 +0000

    load english corpus for statistical analysis

--- services/englishpepper/content/loadfiles.sh
+++ services/englishpepper/content/loadfiles.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 mysqlimport -u root -proot --local --ignore-lines=1 englishpepper nouns0/ideas.txt
+mysqlimport -u root -proot --local --ignore-lines=1 --fields-terminated-by=' ' englishpepper corpus.txt
