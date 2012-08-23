dfed63f687100d132a0c4d7af0ef9d5b475c6e67
Ubuntu <ubuntu@domU-12-31-38-04-6A-5A.compute-1.internal>
2012-08-23 11:54:05 +0000

    Prepare to grind

--- services/englishpepper/content/genstats.sh
+++ services/englishpepper/content/genstats.sh
@@ -8,3 +8,17 @@ mysql -u root -proot englishpepper -e 'SELECT * FROM corpus WHERE pos LIKE "np%"
 mysql -u root -proot englishpepper -e 'SELECT * FROM corpus WHERE pos LIKE "vv%" ORDER BY n2 DESC LIMIT 1000' > verbs0/list.txt
 mysql -u root -proot englishpepper -e 'SELECT * FROM corpus WHERE pos LIKE "aj%" ORDER BY n2 DESC LIMIT 1000' > adj0/list.txt
 mysql -u root -proot englishpepper -e 'SELECT * FROM corpus WHERE pos LIKE "av%" ORDER BY n2 DESC LIMIT 1000' > adv0/list.txt
+
+mkdir -p nouns1 names1 verbs1 adj1 adv1
+mysql -u root -proot englishpepper -e 'SELECT * FROM corpus WHERE pos LIKE "nn%" ORDER BY n2 DESC LIMIT 1000,1000' > nouns1/list.txt
+mysql -u root -proot englishpepper -e 'SELECT * FROM corpus WHERE pos LIKE "np%" ORDER BY n2 DESC LIMIT 1000,1000' > names1/list.txt
+mysql -u root -proot englishpepper -e 'SELECT * FROM corpus WHERE pos LIKE "vv%" ORDER BY n2 DESC LIMIT 1000,1000' > verbs1/list.txt
+mysql -u root -proot englishpepper -e 'SELECT * FROM corpus WHERE pos LIKE "aj%" ORDER BY n2 DESC LIMIT 1000,1000' > adj1/list.txt
+mysql -u root -proot englishpepper -e 'SELECT * FROM corpus WHERE pos LIKE "av%" ORDER BY n2 DESC LIMIT 1000,1000' > adv1/list.txt
+
+mkdir -p nouns2 names2 verbs2 adj2 adv2
+mysql -u root -proot englishpepper -e 'SELECT * FROM corpus WHERE pos LIKE "nn%" ORDER BY n2 DESC LIMIT 2000,1000' > nouns2/list.txt
+mysql -u root -proot englishpepper -e 'SELECT * FROM corpus WHERE pos LIKE "np%" ORDER BY n2 DESC LIMIT 2000,1000' > names2/list.txt
+mysql -u root -proot englishpepper -e 'SELECT * FROM corpus WHERE pos LIKE "vv%" ORDER BY n2 DESC LIMIT 2000,1000' > verbs2/list.txt
+mysql -u root -proot englishpepper -e 'SELECT * FROM corpus WHERE pos LIKE "aj%" ORDER BY n2 DESC LIMIT 2000,1000' > adj2/list.txt
+mysql -u root -proot englishpepper -e 'SELECT * FROM corpus WHERE pos LIKE "av%" ORDER BY n2 DESC LIMIT 2000,1000' > adv2/list.txt
--- services/englishpepper/content/loadfiles.sh
+++ services/englishpepper/content/loadfiles.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-mysqlimport -u root -proot --local --ignore-lines=1 englishpepper nouns0/ideas.txt
+mysqlimport -u root -proot --local --ignore-lines=1 englishpepper */ideas.txt
 mysqlimport -u root -proot --local --ignore-lines=1 --fields-terminated-by=' ' englishpepper corpus.txt
 mysql -u root -proot englishpepper -e 'INSERT INTO corpus(n1,text,pos,n2) SELECT n1,SUBSTRING_INDEX(text,"-",1),pos,n2 FROM corpus WHERE pos LIKE "%-%"'
 mysql -u root -proot englishpepper -e 'INSERT INTO corpus(n1,text,pos,n2) SELECT n1,SUBSTRING_INDEX(text,"-",-1),pos,n2 FROM corpus WHERE pos LIKE "%-%"'
