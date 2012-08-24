c6991469ac184c1098a24bc646d73f109fda5766
Ubuntu <ubuntu@domU-12-31-38-04-6A-5A.compute-1.internal>
2012-08-24 21:43:07 +0000

    cleanup blank records after loading contents

--- services/englishpepper/content/loadfiles.sh
+++ services/englishpepper/content/loadfiles.sh
@@ -2,6 +2,7 @@
 
 mysqlimport -u root -proot --local --ignore-lines=1 englishpepper */ideas.txt
 mysqlimport -u root -proot --local --ignore-lines=1 --fields-terminated-by=' ' englishpepper corpus.txt
+mysql -u root -proot englishpepper -e 'DELETE FROM ideas WHERE wiki=""'
 mysql -u root -proot englishpepper -e 'INSERT INTO corpus(n1,text,pos,n2) SELECT n1,SUBSTRING_INDEX(text,"-",1),pos,n2 FROM corpus WHERE pos LIKE "%-%"'
 mysql -u root -proot englishpepper -e 'INSERT INTO corpus(n1,text,pos,n2) SELECT n1,SUBSTRING_INDEX(text,"-",-1),pos,n2 FROM corpus WHERE pos LIKE "%-%"'
 mysql -u root -proot englishpepper -e 'DELETE FROM corpus WHERE pos LIKE "%-%"'
